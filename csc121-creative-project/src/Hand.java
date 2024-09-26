43d1571879501b4df52598807b32320d49851ca2
cj-whaley <cj.whaley@vikings.berry.edu>
2024-09-25 21:40:55 -0400

    updated hand and bet classes

---
+++ csc121-creative-project/src/Hand.java
@@ -0,0 +1,36 @@
+/**
+ * represents a hand in blackjack, which consists of at least two cards
+ *   but at most 11 cards
+ */
+
+public class Hand {
+	Card card1;
+	Card card2;
+	Card card3;
+	Card card4;
+	Card card5;
+	Card card6;
+	Card card7;
+	Card card8;
+	Card card9;
+	Card card10;
+	Card card11;
+	
+	Hand(Card card1, Card card2, Card card3, Card card4, 
+			Card card5, Card card6, Card card7, Card card8, 
+			Card card9, Card card10, Card card11) {
+		this.card1 = card1;
+		this.card2 = card2;
+		this.card3 = card3;
+		this.card4 = card4;
+		this.card5 = card5;
+		this.card6 = card6;
+		this.card7 = card7;
+		this.card8 = card8;
+		this.card9 = card9;
+		this.card10 = card10;
+		this.card11 = card11;
+	}
+
+	
+}
