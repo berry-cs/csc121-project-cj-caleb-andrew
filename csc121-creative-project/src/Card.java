4afb04e4d71ca82b3e208cd540414695a07e458d
cj-whaley <cj.whaley@vikings.berry.edu>
2024-10-22 20:02:04 -0400

    updated draw method in card class to display the correct card image, and display the back of the card when boolean faceUp is false

--- csc121-creative-project/src/Card.java
+++ csc121-creative-project/src/Card.java
@@ -21,16 +21,246 @@ public class Card implements IWorld {
 		this.faceUp = faceUp;
 	}
 
-	/* draw this card on the given window at the given location */
-	public void draw(PApplet w, Posn cardLoc) {
-		//w.fill(color.getRGB());
-		//w.rectMode(PApplet.CENTER);
-		//w.rect(cardLoc.getX(), cardLoc.getY(), 20, 40);
-		w.imageMode(w.CENTER);
-        w.image(w.loadImage("card back updated.png"), cardLoc.getX(), cardLoc.getY());
 
-		
+	/** returns true if the Card is a Heart */
+	public boolean isHeart() {
+		if(this.suit.equals("Hearts")) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
+
+	/** returns true if the Card is a Diamond */
+	public boolean isDiamond() {
+		if(this.suit.equals("Diamonds")) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	/** returns true if the Card is a Club */
+	public boolean isClub() {
+		if(this.suit.equals("Clubs")) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	/** returns true if the Card is a Spades */
+	public boolean isSpade() {
+		if(this.suit.equals("Spades")) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	/** draw this card on the given window at the given location */
+	public void draw(PApplet w, Posn cardLoc) {
+		w.imageMode(w.CENTER);
+		if(this.faceUp) {
+			if(this.isHeart()) {
+				if(this.rank =='2') {
+					w.image(w.loadImage("2 Hearts.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='3') {
+					w.image(w.loadImage("3 Hearts.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='4') {
+					w.image(w.loadImage("4 Hearts.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='5') {
+					w.image(w.loadImage("5 Hearts.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='6') {
+					w.image(w.loadImage("6 Hearts.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='7') {
+					w.image(w.loadImage("7 Hearts.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='8') {
+					w.image(w.loadImage("8 Hearts.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='9') {
+					w.image(w.loadImage("9 Hearts.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='T') {
+					w.image(w.loadImage("10 Hearts.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='J') {
+					w.image(w.loadImage("Jack Hearts.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='Q') {
+					w.image(w.loadImage("Queen Hearts.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='K') {
+					w.image(w.loadImage("King Hearts.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='A') {
+					w.image(w.loadImage("Ace Hearts.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else {
+
+				}
+			}
+			else if(this.isDiamond()) {
+				if(this.rank =='2') {
+					w.image(w.loadImage("2 Diamonds.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='3') {
+					w.image(w.loadImage("3 Diamonds.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='4') {
+					w.image(w.loadImage("4 Diamonds.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='5') {
+					w.image(w.loadImage("5 Diamonds.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='6') {
+					w.image(w.loadImage("6 Diamonds.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='7') {
+					w.image(w.loadImage("7 Diamonds.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='8') {
+					w.image(w.loadImage("8 Diamonds.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='9') {
+					w.image(w.loadImage("9 Diamonds.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='T') {
+					w.image(w.loadImage("10 Diamonds.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='J') {
+					w.image(w.loadImage("Jack Diamonds.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='Q') {
+					w.image(w.loadImage("Queen Diamonds.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='K') {
+					w.image(w.loadImage("King Diamonds.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='A') {
+					w.image(w.loadImage("Ace Diamonds.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else {
+
+				}
+			}
+			else if(this.isClub()) {
+				if(this.rank =='2') {
+					w.image(w.loadImage("2 Clubs.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='3') {
+					w.image(w.loadImage("3 Clubs.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='4') {
+					w.image(w.loadImage("4 Clubs.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='5') {
+					w.image(w.loadImage("5 Clubs.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='6') {
+					w.image(w.loadImage("6 Clubs.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='7') {
+					w.image(w.loadImage("7 Clubs.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='8') {
+					w.image(w.loadImage("8 Clubs.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='9') {
+					w.image(w.loadImage("9 Clubs.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='T') {
+					w.image(w.loadImage("10 Clubs.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='J') {
+					w.image(w.loadImage("Jack Clubs.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='Q') {
+					w.image(w.loadImage("Queen Clubs.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='K') {
+					w.image(w.loadImage("King Clubs.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='A') {
+					w.image(w.loadImage("Ace Clubs.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else {
+
+				}
+			}
+			else if(this.isSpade()) {
+				if(this.rank =='2') {
+					w.image(w.loadImage("2 Spades.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='3') {
+					w.image(w.loadImage("3 Spades.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='4') {
+					w.image(w.loadImage("4 Spades.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='5') {
+					w.image(w.loadImage("5 Spades.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='6') {
+					w.image(w.loadImage("6 Spades.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='7') {
+					w.image(w.loadImage("7 Spades.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='8') {
+					w.image(w.loadImage("8 Spades.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='9') {
+					w.image(w.loadImage("9 Spades.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='T') {
+					w.image(w.loadImage("10 Spades.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='J') {
+					w.image(w.loadImage("Jack Spades.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='Q') {
+					w.image(w.loadImage("Queen Spades.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='K') {
+					w.image(w.loadImage("King Spades.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else if(this.rank =='A') {
+					w.image(w.loadImage("Ace Spades.png"), cardLoc.getX(), cardLoc.getY());
+				}
+				else {
+
+				}
+			}
+			else {
+
+			}
+		}
+		else {
+			w.image(w.loadImage("card back updated.png"), cardLoc.getX(), cardLoc.getY());
+		}
+	}
+
+	/* draw this card on the given window at the given location */
+	//	public void draw(PApplet w, Posn cardLoc) {
+	//w.fill(color.getRGB());
+	//w.rectMode(PApplet.CENTER);
+	//w.rect(cardLoc.getX(), cardLoc.getY(), 20, 40);
+	//	w.imageMode(w.CENTER);
+	//    w.image(w.loadImage("card back updated.png"), cardLoc.getX(), cardLoc.getY());
+
+
+	//	}
 
 	@Override
 	public String toString() {
@@ -54,8 +284,8 @@ public class Card implements IWorld {
 		return Objects.equals(color, other.color) && faceUp == other.faceUp && rank == other.rank
 				&& Objects.equals(suit, other.suit);
 	}
-	
-	
+
+
 }
 
 
