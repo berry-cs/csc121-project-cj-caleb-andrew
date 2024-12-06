acf95b4464a43e862ccfa85a1df75c6a4d985758
CalebSpikes1 <calebspikes71@gmail.com>
2024-12-06 12:49:13 -0500

    Fixed the addcard class to use array list properly. Also added more tests.

--- csc121-creative-project/src/Hand.java
+++ csc121-creative-project/src/Hand.java
@@ -8,131 +8,109 @@ import processing.core.PApplet;
  *   but at most 11 cards
  */
 
-
-/*
- * NEED REDESIGN
- * deck has been redesigned to use an ArrayList<Card> instead of an ILoC
- * need to redesign constructor and methods to use ArrayList
- */
-
 public class Hand implements IWorld {
-	private ArrayList<Card> cards;
-	private Posn loc;
-	private int total; // represents total value of the cards in the hand
+    private ArrayList<Card> cards;
+    private Posn loc;
+    private int total; // represents total value of the cards in the hand
 
-	Hand(ArrayList<Card> cards, Posn loc) {
-		this.cards = cards;
-		this.loc = loc;
-		total = totalHandVal(cards);
-	}
+    Hand(ArrayList<Card> cards, Posn loc) {
+        this.cards = cards;
+        this.loc = loc;
+        total = totalHandVal(cards);
+    }
 
+    /** draw a picture of the cards at this hand's (x,y) */
+    public PApplet draw(PApplet w) { 
+        for(int i = 0; i < cards.size(); i++) {
+            if(i == 0) {
+                cards.get(i).draw(w, loc);
+            }
+            cards.get(i).draw(w, loc.translate(new Posn(i*20, i*20)));
+        }
+        return w;
+    }
 
-	/** draw a picture of the cards at this hand's (x,y)
-	 * 
-	 */
-	public PApplet draw(PApplet w) { 
-		for(int i = 0; i < cards.size(); i++) {
-			if(i == 0) {
-				cards.get(i).draw(w, loc);
-			}
-			cards.get(i).draw(w, loc.translate(new Posn(i*20, i*20)));
-		}
-		return w;
-	}
+    /** adds the first card from the given deck to the hand 
+     *   and turns the card face up 
+     */
+    public Hand addCard(Deck d) {
+        Card add = d.getCard();
+        cards.add(add.flipCard());
+        total = totalHandVal(cards); // Update the total value of the hand
+        return this;
+    }
 
-	/** adds the first card from the given deck to the hand 
-	 *   and turns the card face up 
-	 */
-	public Hand addCard(Deck d) {
-		Card add = d.getCard();
-		/*
-		if (add.value() == 11 && (total + 11 > 21)) { //it's an ace
-			total = total + 1; //ace adds as a one to not end the game
-		} else {
-			total = total + add.value();
-		}
-		 */
-		cards.addFirst(add.flipCard());
-		return this;
-	}
-	
-	
-	public int total() {
-		return totalHandVal(this.cards);
-	}
-	
-	
-	/**
-	 * computes the total value of all given cards in the hand
-	 * @param cards
-	 * @return
-	 */
-	public int totalHandVal (ArrayList<Card> cards) {
-		int result = 0;
-		int numAces = 0;
-		for(int i = 0; i < cards.size(); i++) {
-			if (cards.get(i).value() == 11) { numAces += 1; }
-			result = result + cards.get(i).value();
-		}
-		
-		while (result > 21 && numAces > 0) {
-			result -= 10;  // treat one ace as a 1 instead of 11 value
-			numAces -= 1;
-		}
-		
-		return result;
-	}
+    public int total() {
+        return totalHandVal(this.cards);
+    }
 
-	@Override
-	public String toString() {
-		return "Hand [cards=" + cards + ", loc=" + loc + "]";
-	}
+    /**
+     * computes the total value of all given cards in the hand
+     * @param cards
+     * @return
+     */
+    public int totalHandVal (ArrayList<Card> cards) {
+        int result = 0;
+        int numAces = 0;
+        for(int i = 0; i < cards.size(); i++) {
+            if (cards.get(i).value() == 11) { numAces += 1; }
+            result = result + cards.get(i).value();
+        }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(cards, loc);
-	}
+        while (result > 21 && numAces > 0) {
+            result -= 10;  // treat one ace as a 1 instead of 11 value
+            numAces -= 1;
+        }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Hand other = (Hand) obj;
-		return Objects.equals(cards, other.cards) && Objects.equals(loc, other.loc);
-	}
+        return result;
+    }
 
-	public ArrayList<Card> getCards() {
-		return this.cards;
-	}
+    @Override
+    public String toString() {
+        return "Hand [cards=" + cards + ", loc=" + loc + "]";
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(cards, loc);
+    }
 
-	public Posn getLoc() {
-		return loc;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Hand other = (Hand) obj;
+        return Objects.equals(cards, other.cards) && Objects.equals(loc, other.loc);
+    }
 
+    public ArrayList<Card> getCards() {
+        return this.cards;
+    }
 
-	public void setLoc(Posn loc) {
-		this.loc = loc;
-	}
+    public Posn getLoc() {
+        return loc;
+    }
 
+    public void setLoc(Posn loc) {
+        this.loc = loc;
+    }
 
-	public int getTotal() {
-		return total;
-	}
+    public int getTotal() {
+        return total;
+    }
 
+    public void setTotal(int total) {
+        this.total = total;
+    }
 
-	public void setTotal(int total) {
-		this.total = total;
-	}
-
-
-	public void setCards(ArrayList<Card> cards) {
-		this.cards = cards;
-	}
+    public void setCards(ArrayList<Card> cards) {
+        this.cards = cards;
+    }
+}
 
 
 
@@ -150,8 +128,6 @@ public class Hand implements IWorld {
 	Card card11;*/
 
 
-
-}
 
 // restructure to make interface with empty hand or list of cards
 // 1) hit method- adds new card- 
